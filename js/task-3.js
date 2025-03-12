589362204e3c5f097c4e5a535ad015de229d1f8b
Ivanka Haidai <ivankia01@ukr.net>
2025-03-12 13:44:09 +0200

    add tasks 1,2,3,4 . js to files

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = Number.parseFloat(content);
-    const paddingWidth = Number.parseFloat(padding) * 2;
-    const borderWidth = Number.parseFloat(border) * 2;
-    return (contentWidth + paddingWidth + borderWidth);
-  }
-  console.log(getElementWidth("50px", "8px", "4px"));
-  console.log(getElementWidth("60px", "12px", "8.5px"));
-  console.log(getElementWidth("200px", "0px", "0px"));
+function checkForSpam(message) {
+  const spam = "spam";
+  const sale = "sale";
+  if (message.toLowerCase().includes(spam) || message.toLowerCase().includes(sale)) {
+    return true;
+  } else { return false };
+}
+
+console.log(checkForSpam("Latest technology news")); 
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
